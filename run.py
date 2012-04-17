9eaff3434cd0da75885a4259186e28725bc52032
Alf <alf.g.jr@gmail.com>
2012-04-16 20:27:59 -0700

    Or this one!

--- run.py
+++ run.py
@@ -2,6 +2,8 @@
 
 import os.path
 import sqlite3
+import sys
+import os
 
 from flask import Flask
 
@@ -9,6 +11,7 @@ from admincraft import config
 from admincraft import views
 
 app = Flask(__name__)
+currentpath = os.path.split(sys.argv[0])[0]
 
 #Registering view module
 app.register_blueprint(views.admincraft)
@@ -18,7 +21,7 @@ print app.url_map
 app.secret_key = config.SECRETKEY
 
 #Create database if it does not exist
-dbpath = config.DATABASE
+dbpath = currentpath + "/" + config.DATABASE
 
 if not os.path.exists(dbpath):
     conn = sqlite3.connect(dbpath)
@@ -28,5 +31,6 @@ if not os.path.exists(dbpath):
     c.close()
 
 if __name__ == "__main__":
+    path = os.path.split(sys.argv[0])[0]
     #Start App
     app.run(host=config.SERVERHOST, port=config.SERVERPORT, debug=config.DEBUGMODE, use_reloader=config.AUTORELOADER)
